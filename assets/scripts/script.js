ba4b6772358c4e18861bb07687bba6ac6bb6a7cc
Nathan Batchelder <natebatchelder@gmail.com>
2022-02-11 18:23:00 -0800

    adding funciton for 'states'

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,7 +8,6 @@ var testTime = 60;
 var score = 0;
 var answerBank = document.getElementById("answerBank");
 var submitScore = document.getElementById("scoreSubmit");
-var userName = document.getElementById("playerName");
 var localStorage = window.localStorage;
 var highscoreLink = document.getElementById("hallOfFame");
 var highscoreDisplay = document.getElementById("scoreboard");
@@ -18,7 +17,6 @@ var createLi = document.createElement("li");
 var scoreBank = [];
 
 let finalScore = document.getElementById("submitScore");
-
 
 // read scores from local store
 var updateHallOfFame = function () {
@@ -36,7 +34,6 @@ var updateHallOfFame = function () {
     orderlistHighScoresEl.appendChild(node);
   });
 };
-
 
 // Any number of questions can be asked in this section for the quiz.  Here, there are ten questions presented with four answer choices per question.
 // A simple way to create a new question type would be for there to be a 'true' and 'false' answer choice with two buttons.
@@ -111,7 +108,7 @@ function setTimer() {
     if (testTime <= 0 || questionNum === questions.length) {
       clearInterval(timerInterval);
       timer.textContent = "GAME OVER";
-      endGame();
+      // endGame();
     }
     testTime--;
   }, 1000);
@@ -160,11 +157,37 @@ answerBank.addEventListener("click", function (event) {
   if (questionNum < questions.length) {
     startTest();
   } else {
-    console.log(`End of Game!`)
-    console.log(`Score is: `, score, `.`, `With `, testTime, `seconds remaining.`);
-    endGame();
+    console.log(`End of Game!`);
+    console.log(
+      `Score is: `,
+      score,
+      `.`,
+      `With `,
+      testTime,
+      `seconds remaining.`
+    );
+
+    displayEndGameDivs();
   }
 });
+
+function displayEndGameDivs() {
+  // display the gameOver div
+  // hide the quiz div
+  // hide the startPage div ("none")
+}
+
+function displayStartPageDiv() {
+  // display the startPage div
+  // hide the gameOver div
+  // hide the quiz div ("none")
+}
+
+function displayScoreboardDiv() {
+  // display the scoreboard div
+  // hide the startPage div ("none")
+  // hide the gameOver div
+}
 
 highscoreLink.addEventListener("click", function () {
   startPage.style.display = "none";
@@ -180,7 +203,7 @@ returnHome.addEventListener("click", function () {
   highscoreDisplay.style.display = "none";
 });
 
-// 
+//
 startButton.addEventListener("click", function () {
   setTimer();
   quizContainer.style.display = "block";
@@ -188,7 +211,7 @@ startButton.addEventListener("click", function () {
   startPage.style.display = "none";
 });
 
-// 
+// ?does this become redundant b/c we have the function displayStartPageDiv()?
 restartButton.addEventListener("click", function () {
   startPage.style.display = "block";
   quizContainer.style.display = "none";
@@ -196,7 +219,7 @@ restartButton.addEventListener("click", function () {
   highscoreDisplay.style.display = "none";
 });
 
-// 
+//
 submitScore.addEventListener("click", function (event) {
   event.preventDefault();
   scoreBank.push(score);
@@ -205,14 +228,24 @@ submitScore.addEventListener("click", function (event) {
 });
 
 function endGame() {
+  // grab the username that was inputed by the user
+  var userName = document.getElementById("playerName").value;
+  // grab the score that was inputed by the user
+  var userScore = document.getElementById("finalScore").value;
+  console.log(`userScore `, userScore);
+  // set the these items to the localStorage
+  localStorage.setItem(userName.value, userScore.value);
+  // key username value score
+
   quizContainer.style.display = "none";
   startPage.style.display = "none";
   gameOverScreen.style.display = "block";
 
-  for (i = 0; i < localStorage.length; i++) {
-    createLi.innerHTML = userName.value + ": " + localStorage.key(i).value;
-    scoreItems.innerHTML = createLi;
-  }
+  // for (i = 0; i < localStorage.length; i++) {
+  //   createLi.innerHTML = userName.value + ": " + localStorage.key(i).value;
+  //   console.log(createLi); // this logs the user's name and score
+  //   scoreItems.innerHTML = createLi;
+  // }
 }
 
 var highscores = JSON.parse(localStorage.getItem("highscores"));
