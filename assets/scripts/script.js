8ce89e82d394a11569567556f7a233f64f4ed9ac
Nathan Batchelder <natebatchelder@gmail.com>
2022-02-11 17:29:44 -0800

    updates to console.log for functinality and live view of user score

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,19 +8,35 @@ var testTime = 60;
 var score = 0;
 var answerBank = document.getElementById("answerBank");
 var submitScore = document.getElementById("scoreSubmit");
-var userInitial = document.getElementById("playerInitials");
+var userName = document.getElementById("playerName");
 var localStorage = window.localStorage;
-var highscoreLink = document.getElementById("highLink");
+var highscoreLink = document.getElementById("hallOfFame");
 var highscoreDisplay = document.getElementById("scoreboard");
-var scoreItems = document.getElementById("scoreitems");
+var scoreItems = document.getElementById("scoreItems");
 var returnHome = document.getElementById("homeReturn");
 var createLi = document.createElement("li");
 var scoreBank = [];
 
+let finalScore = document.getElementById("submitScore");
 
-quizContainer.style.display = "none";
-gameOverScreen.style.display = "none";
-highscoreDisplay.style.display = "none";
+
+// read scores from local store
+var updateHallOfFame = function () {
+  // read local store for highscores array object
+  initialArray = JSON.parse(store.getItem("submitScore"));
+  // if it is first use the just set to an empty array object
+  if (initialArray == null) {
+    initialArray = [];
+    store.setItem("hofScores", JSON.stringify(initialArray));
+  }
+  initialArray.forEach(function (value) {
+    var node = document.createElement("li");
+    var txtNode = document.createTextNode(value.initial + " - " + value.score);
+    node.appendChild(txtNode);
+    orderlistHighScoresEl.appendChild(node);
+  });
+};
+
 
 // Any number of questions can be asked in this section for the quiz.  Here, there are ten questions presented with four answer choices per question.
 // A simple way to create a new question type would be for there to be a 'true' and 'false' answer choice with two buttons.
@@ -89,7 +105,6 @@ var questions = [
   },
 ];
 
-
 function setTimer() {
   var timerInterval = setInterval(function () {
     timer.textContent = testTime;
@@ -104,9 +119,12 @@ function setTimer() {
 
 function startTest() {
   var question = questions[questionNum];
-  console.log("startTest");
-  console.log(question);
+  // this logs that a new question is being asked
+  console.log("Next Question!");
+  // this logs the question number being asked
   console.log(questionNum);
+  // this logs the question being asked
+  console.log(`the question reads: `, question);
 
   questionPrompt.textContent = questions[questionNum].prompt;
 
@@ -118,7 +136,7 @@ function startTest() {
   button2.textContent = questions[questionNum].answers[1];
   button3.textContent = questions[questionNum].answers[2];
   button4.textContent = questions[questionNum].answers[3];
-} 
+}
 
 answerBank.addEventListener("click", function (event) {
   userChoice = event.target;
@@ -128,14 +146,24 @@ answerBank.addEventListener("click", function (event) {
     questions[questionNum].answers[questions[questionNum].correct]
   ) {
     score += 10;
-    console.log(score);
+    console.log(`CORRECT! Score increases by ten points to: `, score);
+    console.log(`Current /SCORE/ is: `, score);
+    console.log(`Current /TIME/ remaining is `, testTime);
   } else {
     testTime -= 5;
-    console.log(score);
+    console.log(`INCORRECT! Time decreaces by 5 seconds to: `, testTime);
+    console.log(`Current /SCORE/ is: `, score);
+    console.log(`Current /TIME/ remaining is `, testTime);
   }
   questionNum++;
-  startTest();
-  console.log(questionNum);
+
+  if (questionNum < questions.length) {
+    startTest();
+  } else {
+    console.log(`End of Game!`)
+    console.log(`Score is: `, score, `.`, `With `, testTime, `seconds remaining.`);
+    endGame();
+  }
 });
 
 highscoreLink.addEventListener("click", function () {
@@ -152,12 +180,23 @@ returnHome.addEventListener("click", function () {
   highscoreDisplay.style.display = "none";
 });
 
+// 
 startButton.addEventListener("click", function () {
   setTimer();
   quizContainer.style.display = "block";
   startTest();
   startPage.style.display = "none";
 });
+
+// 
+restartButton.addEventListener("click", function () {
+  startPage.style.display = "block";
+  quizContainer.style.display = "none";
+  gameOverScreen.style.display = "none";
+  highscoreDisplay.style.display = "none";
+});
+
+// 
 submitScore.addEventListener("click", function (event) {
   event.preventDefault();
   scoreBank.push(score);
@@ -165,14 +204,13 @@ submitScore.addEventListener("click", function (event) {
   endGame();
 });
 
-
 function endGame() {
   quizContainer.style.display = "none";
   startPage.style.display = "none";
   gameOverScreen.style.display = "block";
 
   for (i = 0; i < localStorage.length; i++) {
-    createLi.innerHTML = userInitial.value + ": " + localStorage.key(i).value;
+    createLi.innerHTML = userName.value + ": " + localStorage.key(i).value;
     scoreItems.innerHTML = createLi;
   }
 }
